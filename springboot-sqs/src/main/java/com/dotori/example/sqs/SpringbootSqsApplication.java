16c05285cb3532a56f0e3db5ab207e1e1fee2194
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-05-02 19:17:24 +0900

    cloud 환경설정 외부로 이동.

--- springboot-sqs/src/main/java/com/dotori/example/sqs/SpringbootSqsApplication.java
+++ springboot-sqs/src/main/java/com/dotori/example/sqs/SpringbootSqsApplication.java
@@ -1,12 +1,16 @@
 package com.dotori.example.sqs;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringbootSqsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootSqsApplication.class, args);
+        new SpringApplicationBuilder(SpringbootSqsApplication.class)
+            .properties(
+                "spring.config.location="
+                    + "file:C:\\Users\\joonghyun\\Documents\\config\\application.yml")
+            .run(args);
     }
 }
