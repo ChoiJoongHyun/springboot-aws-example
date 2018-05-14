ad948ab6b9b58f769d89f971ae392bde853aec42
ChoiJoongHyun <joonghyun0117@gmail.com>
2018-05-14 11:45:40 +0900

    add amazonmq project

---
+++ springboot-amazonmq/src/main/java/com/dotori/example/amazonmq/SpringbootAmazonmqApplication.java
@@ -0,0 +1,12 @@
+package com.dotori.example.amazonmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAmazonmqApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAmazonmqApplication.class, args);
+  }
+}
